a0f759fddb9880c39ff8cd72ae5436c7247a5f3b
TektaTewodros0497 <149379332+TektaTewodros0497@users.noreply.github.com>
2023-11-18 07:23:01 +0300

    Update Tekta_tewodros_ETS1299_15

---
+++ Tekta_tewodros_ETS1299_15/BMI.cpp
@@ -0,0 +1,13 @@
+//BMI calculator 
+#include <iostream>
+Using namespace std;
+
+Int main(){
+Float weight, height, BMI;
+Cout<<”enter the weight”;
+Cin>>weight;
+Cout<<”enter the height”;
+Cin>>height;
+Cout<<”Body mass index(BMI) of a person is “<<weight/(height*height)<<endl;
+Return 0;
+}  
